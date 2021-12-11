844db66b96071add93d699744ac0260616ea3d14
Emilio Calvet <emiliocalvet@gmail.com>
2021-12-10 22:23:39 -0300

    ...

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { SculptureController } from './sculpture/sculpture.controller';
+import { SculptureService } from './sculpture/sculpture.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [SculptureController],
+  providers: [SculptureService],
 })
 export class AppModule {}
